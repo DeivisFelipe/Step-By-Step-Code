000b3ed7117f38f05ef27be759d1576c084d61f5
DeivisFelipe <guerreirodosul01@gmail.com>
2025-08-28 14:57:14 -0300

    feat: importanto o html pelo provider

--- src/webview/editor.js
+++ src/webview/editor.js
@@ -4,8 +4,8 @@ let editor;
 require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.45.0/min/vs' } });
 require(['vs/editor/editor.main'], function () {
     editor = monaco.editor.create(document.getElementById('editor'), {
-        value: ``,
-        language: 'javascript',
+        value: `{{CONTENT}}`,
+        language: '{{LANGUAGE}}',
         theme: 'vs-dark',
         automaticLayout: true
     });
